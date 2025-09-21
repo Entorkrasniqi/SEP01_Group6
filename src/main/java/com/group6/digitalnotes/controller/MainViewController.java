9b0fe10dd0262866d45081788b6b91ff55482595
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-09-21 21:26:37 +0300

    file creation and some setups

---
+++ src/main/java/com/group6/digitalnotes/controller/MainViewController.java
@@ -0,0 +1,22 @@
+package com.group6.digitalnotes.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML private BorderPane mainLayout;
+    @FXML private VBox sideBar;
+    @FXML private Button newNoteBtn;
+    @FXML private Button allNotesBtn;
+
+
+}
---
+++ src/main/java/com/group6/digitalnotes/controller/NoteEditorController.java
@@ -0,0 +1,15 @@
+package com.group6.digitalnotes.controller;
+
+import com.group6.digitalnotes.model.Note;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ResourceBundle;
+
+public class NoteEditorController {
+}
