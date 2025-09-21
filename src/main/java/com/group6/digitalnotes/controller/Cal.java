68de1e6b18ea465414a0388d6874323e1aa1b537
Omar <aldulaimiomar111@gmail.com>
2025-09-21 20:12:19 +0300

    test

---
+++ src/main/java/com/group6/digitalnotes/controller/Cal.java
@@ -0,0 +1,10 @@
+package com.group6.digitalnotes.controller;
+
+public class Cal {
+
+    // Example method to test
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
---
+++ src/test/java/com/group6/digitalnotes/controller/CalTest.java
@@ -0,0 +1,12 @@
+package com.group6.digitalnotes.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalTest {
+
+    @Test
+    void add() {
+    }
+}
